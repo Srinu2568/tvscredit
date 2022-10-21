1902ef47f4deffdadf14b67c03c0fc2ce25d85cc
srinivas <mnvsrinivas1@gmail.com>
2022-10-21 17:53:39 +0530

    Find solution for images carousel problem

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from multiprocessing import allow_connection_pickling
 import streamlit as st
 import cv2
 import numpy as np
@@ -13,6 +14,8 @@ import datetime as dt
 import streamlit_authenticator as stauth
 import database as db
 from datetime import datetime
+import streamlit.components.v1 as components
+import base64
 
 load_dotenv('.env')
 
@@ -611,12 +614,54 @@ if authentication_status and db.get_user(username)['isEval']:
         arr_im = [res_image, res_image2]
         im = drive.get(res_image)
         im2 = drive.get(res_image2)
-        file_bytes = np.asarray(bytearray(im.read()), dtype=np.uint8) # Converting image(deta object) to bytearray using numpy
-        opencv_image = cv2.imdecode(file_bytes, 1) # Decoding the bytearray to image(Byte stream)
-        file_bytes2 = np.asarray(bytearray(im2.read()), dtype=np.uint8)
-        opencv_image2 = cv2.imdecode(file_bytes2, 1)
-        arr = [opencv_image, opencv_image2]
-        st.image(arr, channels = 'BGR', output_format='PNG', width=150)
+        # with open(im, 'rb') as image2string:
+        contents = im.read()
+        data_url = base64.b64encode(contents).decode('utf-8')
+        contents2 = im2.read()
+        data_url2 = base64.b64encode(contents2).decode('utf-8')
+        # file_bytes = np.asarray(bytearray(im.read()), dtype=np.uint8) # Converting image(deta object) to bytearray using numpy
+        # cs = base64.b64encode(im.read())
+        # st.write()
+        # opencv_image = cv2.imdecode(file_bytes, 1) # Decoding the bytearray to image(Byte stream)
+        # file_bytes2 = np.asarray(bytearray(im2.read()), dtype=np.uint8)
+        # opencv_image2 = cv2.imdecode(file_bytes2, 1)
+        # arr = [opencv_image, opencv_image2]
+        # st.image(arr, channels = 'BGR', output_format='PNG', width=150)
+        components.html(
+            f"""
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img class="d-block w-100" src="data:image/png;base64, {data_url}" alt="First slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100" src="data:image/png;base64,{data_url2}" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100" src="https://www.princeton.edu/sites/default/files/styles/half_2x/public/images/2022/02/KOA_Nassau_2697x1517.jpg?itok=iQEwihUn" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>""",
+            height=600, width=600
+        )
     
     # Car
     car_form = {}
--- new.py
+++
@@ -1,8 +0,0 @@
-import database as db
-l = db.fetch_all_users()
-a = [x for x in l if 'car' in x['type_data'] and not x['isEval']]
-# print(a[0]['form_data'][1])
-# print(a[0]['key'])
-k = [{l['name']:(l['form_data'], l['images'])} for l in a]
-j = [list(x.keys())[0] for x in k]
-print(j)
