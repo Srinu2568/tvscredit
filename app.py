d8660d3c6aa397fbee61c34ffde391166a65dca0
srinivas <mnvsrinivas1@gmail.com>
2022-10-21 22:53:34 +0530

    Version 2.3.0 Use bootstrap for image carousel

--- app.py
+++ app.py
@@ -631,11 +631,12 @@ if authentication_status and db.get_user(username)['isEval']:
             f"""
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
+        <style>
+            .carousel {{
+            width:640px;
+            height:360px;
+            }}
+        </style>
         <div class="carousel-inner">
             <div class="carousel-item active">
             <img class="d-block w-100" src="data:image/png;base64, {data_url}" alt="First slide">
@@ -660,7 +661,7 @@ if authentication_status and db.get_user(username)['isEval']:
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
             <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>""",
-            height=600, width=600
+            height=350, width=600
         )
     
     # Car
@@ -699,7 +700,8 @@ if authentication_status and db.get_user(username)['isEval']:
                 
                         
         #######################################
-                for i in usercars:
+                if len(usercars) != 0:
+                    i = usercars[0]
                     st.write(f'Car : {i["Car"]}')
                     st.write(f'Fuel Type : {i["Fuel_Type"]}')
                     st.write(f'Kilometers Driven : {i["Kilometers_Driven"]}')
@@ -715,26 +717,76 @@ if authentication_status and db.get_user(username)['isEval']:
                     test_user = db.get_user(desired_user) # Getting user details
                     user_res = db.get_user(desired_user) # Getting cur user details
                     user_images = user_res['images'] # Getting the name of images from user_db
-                    res_image = test_user['images'] # Getting image name from deta drive
-                    arr2 = []
+                    # res_image = test_user['images'] # Getting image name from deta drive
+                    arr2 = [] # Contains user specific images
                     for j in range(len(user_images)):
                         str_manip = f'car-{i["Car"]}-'
                         if user_images[j].startswith(str_manip):
                             arr2.append(user_images[j])
-                    arr = []
-                    for k in range(len(arr2)):
-                        im = drive.get(arr2[k])
-                        file_bytes = np.asarray(bytearray(im.read()), dtype=np.uint8) # Converting image(deta object) to bytearray using numpy
-                        opencv_image = cv2.imdecode(file_bytes, 1) # Decoding the bytearray to image(Byte stream)
-                        arr.append(opencv_image)
-                    st.image(arr, channels = 'BGR', output_format='PNG', width=150)
-                        
-                    # im = drive.get(res_image)
-                    # im2 = drive.get(res_image2)
-                    # file_bytes2 = np.asarray(bytearray(im2.read()), dtype=np.uint8)
-                    # opencv_image2 = cv2.imdecode(file_bytes2, 1)
-                    # arr = [opencv_image, opencv_image2]
-                    # st.image(arr, channels = 'BGR', output_format='PNG', width=150)
+                    if len(arr2) != 0:
+                        arr = [] # Contains actual images
+                        mark2 = """  """     # html dynamic image generate string
+                        for k in range(len(arr2)):
+                            im = drive.get(arr2[k])
+                            contents = im.read()
+                            data_url = base64.b64encode(contents).decode('utf-8')
+                            if k == 0:
+                                des_str = f""" <div class="carousel-item active">
+                                <img class="d-block w-100" src="data:image/png;base64, {data_url}" alt="First slide">
+                                </div> """
+                            elif k != 0:
+                                des_str = f""" <div class="carousel-item">
+                                <img class="d-block w-100" src="data:image/png;base64, {data_url}" alt="First slide">
+                                </div> """
+                            mark2 += des_str
+
+
+
+                        #########################################################################################    
+                        mark1 = """ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+                                    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                                <style>
+                                    .carousel {{
+                                    width:640px;
+                                    height:360px;
+                                    }}
+                                </style>
+                                <div class="carousel-inner"> """
+                        mark3 = """ </div>
+                                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                                </div>
+
+                                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                                    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+                                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> """
+                    
+                        # html rendering
+                        res_html = mark1 + mark2 + mark3
+                        components.html(res_html, height=350, width=600)
+
+                        #     file_bytes = np.asarray(bytearray(im.read()), dtype=np.uint8) # Converting image(deta object) to bytearray using numpy
+                        #     opencv_image = cv2.imdecode(file_bytes, 1) # Decoding the bytearray to image(Byte stream)
+                        #     arr.append(opencv_image)
+                        # st.image(arr, channels = 'BGR', output_format='PNG', width=150)
+
+                        # im = drive.get(res_image)
+                        # im2 = drive.get(res_image2)
+                        # file_bytes2 = np.asarray(bytearray(im2.read()), dtype=np.uint8)
+                        # opencv_image2 = cv2.imdecode(file_bytes2, 1)
+                        # arr = [opencv_image, opencv_image2]
+                        # st.image(arr, channels = 'BGR', output_format='PNG', width=150)
+                    
+                    else:
+                        st.warning("User have not uploaded any images")
+                else:
+                    st.warning("No cars to evaluate for the selected user")
 
 
         
