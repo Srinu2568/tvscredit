9014e2293e6dce34645c218c93f0331bbff21e67
srinivas <mnvsrinivas1@gmail.com>
2022-10-27 14:10:31 +0530

    Version 2.9.1 add css to evaluated vehicles

--- app.py
+++ app.py
@@ -1056,15 +1056,70 @@ if authentication_status and db.get_user(username)['isEval']:
                 des_user_form_data = des_user_data[user][0]
                 final_data = [x for x in des_user_form_data if x['isEvaluated']]
                 # Bootstrap cards and collapse combined for evaluated vehicles
-                mark1 = """ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> """
+                mark1 = """ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> 
+                        <style>
+                            body{
+                                margin-top:20px;
+                                background:#FAFAFA;
+                            }
+                            .order-card {
+                                color: #fff;
+                            }
+
+                            .bg-c-blue {
+                                background: linear-gradient(45deg,#4099ff,#73b4ff);
+                            }
+
+                            .bg-c-green {
+                                background: linear-gradient(45deg,#2ed8b6,#59e0c5);
+                            }
+
+                            .bg-c-yellow {
+                                background: linear-gradient(45deg,#FFB64D,#ffcb80);
+                            }
+
+                            .bg-c-pink {
+                                background: linear-gradient(45deg,#FF5370,#ff869a);
+                            }
+
+
+                            .card {
+                                border-radius: 5px;
+                                -webkit-box-shadow: 0 1px 2.94px 0.06px rgba(4,26,55,0.16);
+                                box-shadow: 0 1px 2.94px 0.06px rgba(4,26,55,0.16);
+                                border: none;
+                                margin-bottom: 30px;
+                                -webkit-transition: all 0.3s ease-in-out;
+                                transition: all 0.3s ease-in-out;
+                            }
+
+                            .card .card-body {
+                                padding: 25px;
+                            }
+
+                            .order-card i {
+                                font-size: 26px;
+                            }
+
+                            .f-left {
+                                float: left;
+                            }
+
+                            .f-right {
+                                float: right;
+                            }
+                        </style>
+                        """
                 mark3 = """ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
                                         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-                                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> """
+                                        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+                                        <script>$("#demoAccordion").accordion({ collapsible: true, active: false});</script> """
+                                        
                 for form in final_data:
                     if form['Type'] == 'car':
                         mark2 = f""" <div id="accordion">
 
-                            <div class="card">
+                            <div class="card bg-c-blue order-card">
                                 <div class="card-header" id="headingOne">
                                 <h5 class="mb-0">
                                     <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
@@ -1082,7 +1137,7 @@ if authentication_status and db.get_user(username)['isEval']:
                                     </blockquote>
                                 </div>
 
-                                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                                <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
                                 <div class="card-body">
                                     <div><b>Description of Car</b></div>
                                     <div><em>Car Model : {form['Car']} </em></div>
@@ -1103,7 +1158,7 @@ if authentication_status and db.get_user(username)['isEval']:
                     if form['Type'] == 'bike':
                         mark2 = f""" <div id="accordion">
 
-                            <div class="card">
+                            <div class="card bg-c-green order-card">
                                 <div class="card-header" id="headingOne">
                                 <h5 class="mb-0">
                                     <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
@@ -1112,7 +1167,7 @@ if authentication_status and db.get_user(username)['isEval']:
                                 </h5>
                                 </div>
 
-                                <div class="card-body">
+                                <divrr class="card-body">
                                     <blockquote class="blockquote mb-0">
                                     <div><i><small>City : {form['City']}</small></i></div>
                                         <div><i><small>Year : {form['Year']}</small></i></div>
@@ -1121,7 +1176,7 @@ if authentication_status and db.get_user(username)['isEval']:
                                     </blockquote>
                                 </div>
 
-                                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                                <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
                                 <div class="card-body">
                                     <div><b>Description of Bike</b></div>
                                     <div><em>Bike Model : {form['Bike']} </em></div>
@@ -1139,7 +1194,7 @@ if authentication_status and db.get_user(username)['isEval']:
                                 </div>
                             </div> """
                     mark_html = mark1+mark2+mark3
-                    components.html(mark_html, height=200, scrolling=True)
+                    components.html(mark_html, height=300, scrolling=True)
 
 # Auth edge cases
 elif authentication_status == False:
