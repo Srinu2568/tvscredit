655342abb8f3f38130c26d14b5e3af78e1156dc7
srinivas <mnvsrinivas1@gmail.com>
2022-11-06 17:54:34 +0530

    Update unicode quotes to single quotes

--- setup.sh
+++ setup.sh
@@ -12,11 +12,11 @@ enableCORS=false\n\
 port = $PORT\n\
 
 [theme]
-primaryColor = ‘#84a3a7’
-backgroundColor = ‘#EFEDE8’
-secondaryBackgroundColor = ‘#fafafa’
-textColor= ‘#424242’
-font = ‘sans serif’
+primaryColor = '#84a3a7'
+backgroundColor = '#EFEDE8'
+secondaryBackgroundColor = '#fafafa'
+textColor= '#424242'
+font = 'sans serif'
 [server]
 headless = true
 port = $PORT
