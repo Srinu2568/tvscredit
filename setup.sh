23abda45cb99d30b61c63d38d003b029c9379641
Srinivas Menta <86238631+Srinu2568@users.noreply.github.com>
2022-11-06 17:49:38 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"mnvsrinivas1@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
@@ -10,4 +10,15 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+
+[theme]
+primaryColor = ‘#84a3a7’
+backgroundColor = ‘#EFEDE8’
+secondaryBackgroundColor = ‘#fafafa’
+textColor= ‘#424242’
+font = ‘sans serif’
+[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
