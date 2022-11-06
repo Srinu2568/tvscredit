6139455cbbf2b8f71a8ffedc907e32c6e4165d9a
srinivas <mnvsrinivas1@gmail.com>
2022-11-06 18:00:14 +0530

    Update setup

--- setup.sh
+++ setup.sh
@@ -10,15 +10,4 @@ echo "\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-
-[theme]
-primaryColor = '#84a3a7'
-backgroundColor = '#EFEDE8'
-secondaryBackgroundColor = '#fafafa'
-textColor= '#424242'
-font = 'sans serif'
-[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
+> ~/.streamlit/config.toml
